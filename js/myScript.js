4e64f0c9a7eef21df6523d06d9dc8316192e4e70
anaseinea <u14111377@sharjah.ac.ae>
2017-05-02 23:41:49 +0400

    adding footer and stuff

--- js/myScript.js
+++ js/myScript.js
@@ -26,9 +26,14 @@
                 products.innerHTML += '<div class="col s12 m6 l4"><div class="card hoverable">'
                +' <div class="card-image waves-effect waves-block waves-light">'
                   +'<img class="activator" src="https://firebasestorage.googleapis.com/v0/b/it-in-e-comm.appspot.com/o/'+prods[i].image 
-                      +'?alt=media&token=e3874610-eac5-4454-903e-082deb5e2bbb"></div>'
+                      +'?alt=media&token=e3874610-eac5-4454-903e-082deb5e2bbb">'
+                    +'<a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">shopping_cart</i></a>'
+                    +'</div>'
                 +'<div class="card-content">'
-                  +'<span class="card-title activator grey-text text-darken-4" style="font-size:1.2rem;">'+prods[i].disc+'<i class="material-icons right">more_vert</i></span><p style="color: orange;">'+prods[i].price+' AED</p>'
+                  +'<span class="card-title activator grey-text text-darken-4" style="font-size:1.2rem;">'+prods[i].disc+
+                   + '<i class="material-icons right">more_vert</i></span>'
+                    
+                    +'<p style="color: orange;">'+prods[i].price+' AED</p>'
                   +'</div><div class="card-reveal">'
                   +'<span class="card-title grey-text text-darken-4">Discription<i class="material-icons right">close</i></span>'
                   +'<p>'+prods[i].discL+'</p>'
@@ -39,4 +44,7 @@
 }());
 
 
-$( document ).ready(function(){$(".button-collapse").sideNav();})
+$( document ).ready(function(){$(".button-collapse").sideNav();
+                              $('.modal').modal();
+                              
+                              })
